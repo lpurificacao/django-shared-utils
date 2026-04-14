f5a087dbf97613ecd58f1828833c27fa02819eed
Luís Purificação <44589966+lpurificacao@users.noreply.github.com>
2026-04-14 18:23:22 +0200

    DocString added

--- utils.py
+++ utils.py
@@ -3,6 +3,29 @@ from django.db.models.query import QuerySet
 
 
 def patch_queryset_pretty_sql():
+    '''If you want this available across your Django project without remembering to call it
+     in every script, run the patch during app startup, such as in an app’s AppConfig.ready() method.
+    Then in INSTALLED_APPS, use that app config so the patch is applied automatically when Django starts.
+
+
+    
+    # myapp/apps.py
+    from django.apps import AppConfig
+
+       class MyAppConfig(AppConfig):
+        name = "myapp"
+
+           def ready(self):
+            from .queryset_patch import patch_queryset_pretty_sql
+            patch_queryset_pretty_sql()
+    
+    # myproject/settings.py
+    INSTALLED_APPS = [
+    ...
+        # local
+    "myapp.apps.BlogConfig",
+    '''
+    
     original_init = QuerySet.__init__
 
     def new_init(self, *args, **kwargs):
